5453c3de6434e94a61502f7e1e28f035f973c9d3
sukratu <67482411+sukratu@users.noreply.github.com>
2024-11-13 22:25:50 +0530

    first commit

--- transaction.sh
+++ transaction.sh
@@ -6,10 +6,11 @@ transaction_type=$2
 amount=$3
 
 # Get the current timestamp
-timestamp= #todo
+#timestamp= $(date '+%Y-%m-%d %H:%M:%S')
 
 # Log the transaction in transaction_log.txt
-#todo
+TRANSACTION_ENTRY="$timestamp | Account: $account_number | Transaction: $transaction_type | Amount: $amount"
 
+echo "$TRANSACTION_ENTRY" >> transaction_log.txt
 # Check if the transaction is a high-value withdrawal
 #todo
