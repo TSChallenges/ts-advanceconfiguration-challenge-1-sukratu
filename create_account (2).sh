d6d1489aabf49df415b10bf6523cfc2774f59070
sukratu <67482411+sukratu@users.noreply.github.com>
2024-11-13 22:33:10 +0530

    Add files via upload

---
+++ create_account (2).sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Input parameters: username and account number
+username=$1
+account_number=$2
+
+# Default balance for new accounts
+default_balance= #todo
+tess
+# Check if account already exists in accounts.txt
+#todo
+
+# Add the new account to accounts.txt
+#todo
+
+# Create the user in the system (if running in a real Linux environment)
+#todo
+
